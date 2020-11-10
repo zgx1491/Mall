343fdafd7c6e7e123bf3d402058bc6a32268ca01
zgx1491 <1491256020@qq.com>
2020-11-10 18:31:34 +0800

    'update'

---
+++ src/router/index.js
@@ -0,0 +1,37 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+const Home = () => import('../views/home/Home')
+const Category = () => import('../views/category/Category')
+const Shopcart = () => import('../views/shopcart/Shopcart')
+const Profile = () => import('../views/profile/Profile')
+
+
+const router = new VueRouter({
+  routes: [
+    {
+      path: '/',
+      redirect: '/home'
+    },
+    {
+      path: '/home',
+      component: Home
+    },
+    {
+      path: '/category',
+      component: Category
+    },
+    {
+      path: '/shopcart',
+      component: Shopcart
+    },
+    {
+      path: '/profile',
+      component: Profile
+    },
+  ],
+  mode: 'history'
+})
+
+export default router
